79bb68aacbd5c85c390f8c09bea73f7e22612458
Ro5hi <slen.detablan@gmail.com>
2021-03-16 13:36:17 -0600

    added navbar and left bar, styled to align content

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ function App() {
     <div class="wall">
       <div class="nav">
         <div class="nav-bar">
-          <button onClick={}>Facebook</button>
-          <button onClick={}>Search</button>
-          <button onClick={}>Home</button>
-          <button onClick={}>Pages</button>
-          <button onClick={}>Watch</button>
-          <button onClick={}>Market</button>
-          <button onClick={}>Home</button>
-          <button onClick={}>Create</button>
-          <button onClick={}>Messages</button>
-          <button onClick={}>Notifications</button>
+          <button onClick={""}>Facebook</button>
+          <button onClick={""}>Search</button>
+          <button onClick={""}>Home</button>
+          <button onClick={""}>Pages</button>
+          <button onClick={""}>Watch</button>
+          <button onClick={""}>Market</button>
+          <button onClick={""}>Home</button>
+          <button onClick={""}>Create</button>
+          <button onClick={""}>Messages</button>
+          <button onClick={""}>Notifications</button>
+          <button onClick={""}>Account</button>
+        </div>
+      </div>
+      <div class="user-bar">
+        <h4>Name</h4>
+        <button onClick={""}>Friends</button>
+        <button onClick={""}>Groups</button>
+        <button onClick={""}>Memories</button>
+        <button onClick={""}>See More</button>
+        <div class="shortcuts">
+          <button onClick={""}>Example Page</button>
+          <button onClick={""}>Example Group</button>
+          <button onClick={""}>See More</button>
         </div>
       </div>
     </div>
