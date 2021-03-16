6167be93bec6c92da33b1fea07cba3c91df02375
= <=>
2021-03-16 13:09:44 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <div class="wall">
-      
+      <div class="nav">
+        <div class="nav-bar">
+          <button onClick={}>Facebook</button>
+          <button onClick={}>Search</button>
+          <button onClick={}>Home</button>
+          <button onClick={}>Pages</button>
+          <button onClick={}>Watch</button>
+          <button onClick={}>Market</button>
+          <button onClick={}>Home</button>
+          <button onClick={}>Create</button>
+          <button onClick={}>Messages</button>
+          <button onClick={}>Notifications</button>
+        </div>
+      </div>
     </div>
   );
 }
